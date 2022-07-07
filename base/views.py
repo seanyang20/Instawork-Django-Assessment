0925aa93c398c732ca444b8c590c9c91b5594941
Sean <sean.yang@econommi.io>
2022-07-06 22:59:00 -0700

    rendered preliminary data of team members onto list page & moved add edit list pages to templates inside our app folder and keep our main page inside our root templates folder

--- base/views.py
+++ base/views.py
@@ -2,8 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+team_members = [
+    {'id':1, 'name':'Adrien Olczak (admin)', 'number': '415-310-619', 'email': 'adrien@instawork.com'},
+    {'id':2, 'name':'Charlene Pham', 'number': '415-310-619', 'email': 'charlene@instawork.com'},
+    {'id':3, 'name':'Benson Mach', 'number': '415-310-619', 'email': 'benson@instawork.com'},
+    {'id':4, 'name':'Dan Petrie', 'number': '415-310-619', 'email': 'dan@instawork.com'}
+]
+
+
+
 def list(request):
-    return render(request, 'list.html')
+    context = {'team_members': team_members}
+    return render(request, 'list.html', context)
 
 def add(request):
     return render(request, 'add.html')
--- templates/list.html
+++
@@ -1,1 +0,0 @@
-<h1>List Page</h1>
