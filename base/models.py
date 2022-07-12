ea35fe7b7f8a2f0ba7561b4a580ea2e2b248b49f
Sean <sean.yang@econommi.io>
2022-07-11 20:59:27 -0700

    added region attribute to phone number

--- base/models.py
+++ base/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
+from phonenumber_field.widgets import PhoneNumberPrefixWidget
 from multiselectfield import MultiSelectField
 
 # Create your models here.
@@ -11,7 +12,7 @@ ROLE_CHOICE = ((1, 'Regular'),
 class Member(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    phone = PhoneNumberField(null=False, blank=False, unique=True)
+    phone = PhoneNumberField(null=False, blank=False, unique=True, region='US')
     email = models.EmailField(max_length = 254)
     role = MultiSelectField(choices=ROLE_CHOICE)
     updated = models.DateTimeField(auto_now=True)
